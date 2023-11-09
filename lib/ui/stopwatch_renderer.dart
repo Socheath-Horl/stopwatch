04efd127750746a5ef0b7bba3535fb790b4c1c3b
Socheath Horl <horl.socheath@gmail.com>
2023-11-09 21:36:21 +0700

    render clock

--- lib/ui/stopwatch_renderer.dart
+++ lib/ui/stopwatch_renderer.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:stopwatch/ui/clock_hand.dart';
+import 'package:stopwatch/ui/clock_markers.dart';
+import 'package:stopwatch/ui/clock_text_marker.dart';
 
 import 'elapsed_time_text.dart';
 
@@ -18,12 +20,18 @@ class StopwatchRenderer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            border: Border.all(width: 3, color: Colors.orange),
-            borderRadius: BorderRadius.circular(radius)
+        for (var i = 0; i < 60; i++)
+          Positioned(
+            left: radius,
+            top: radius,
+            child: ClockSecondsTickMarker(seconds: i, radius: radius),
           ),
-        ),
+        for (var i = 5; i <= 60; i += 5)
+          Positioned(
+            top: radius,
+            left: radius,
+            child: ClockTextMarker(value: i, maxValue: 60, radius: radius),
+          ),
         Positioned(
           left: radius,
           top: radius,
